25be71b7d54a5c39dc0a41ab37e4c60f81b43d8a
ask2amit <amit.velocity14@gmail.com>
2022-09-18 15:36:56 -0700

    Dashboardclass added

---
+++ Testng/Testng/Dashboard.java
@@ -0,0 +1,14 @@
+package Testng.Testng;
+
+import org.testng.annotations.Test;
+
+public class Dashboard {
+	
+	@Test
+	
+	public void TestA()
+	{
+		System.out.println("This is DashboardClass");
+	}
+
+}
